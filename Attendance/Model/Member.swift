5e447cc013e9675c5c2142529a27d377e31c5abf
Cal Stephens <cal@calstephens.tech>
2016-09-15 17:19:12 -0400

    Basic design prototype

---
+++ Attendance/Model/Member.swift
@@ -0,0 +1,79 @@
+//
+//  Member.swift
+//  Attendance
+//
+//  Created by Cal Stephens on 9/15/16.
+//  Copyright © 2016 Cal Stephens. All rights reserved.
+//
+
+import AppKit
+
+enum Attendance {
+    case Present, Excused, Absent
+}
+
+func ==(left: Member, right: Member) -> Bool {
+    return left.description == right.description
+}
+
+class Member : CustomStringConvertible, Equatable {
+    
+    //MARK: - Load from disk
+    
+    static var all: [Member] {
+        guard let path = Bundle.main.path(forResource: "People", ofType: "csv") else { return [] }
+        let url = URL(fileURLWithPath: path)
+        guard let contents = try? NSString(contentsOf: url, encoding: String.Encoding.utf8.rawValue) else { return [] }
+        
+        let lines = contents.components(separatedBy: "\n").filter{ $0.contains(",") }
+        
+        let people = lines.map { line -> Member in
+            //format: "Cal Stephens, 810"
+            let cols = line.components(separatedBy: ", ")
+            
+            let name = cols[0]
+            let nameComponents = name.components(separatedBy: " ")
+            let firstName = nameComponents[0]
+            let lastName = nameComponents[1]
+            
+            let rollbookString = cols[1]
+            let rollbook = Int(rollbookString)
+            
+            return Member(firstName: firstName, lastName: lastName, rollbook: rollbook)
+        }
+        
+        return people
+    }
+    
+    
+    //MARK: - Properties
+    
+    let firstName: String
+    let lastName: String
+    
+    let rollbook: Int?
+    
+    let attendance: [String : Attendance]
+    var attendanceToday: Attendance?
+    
+    init(firstName: String, lastName: String, rollbook: Int?) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.rollbook = rollbook
+        
+        attendance = [:]
+    }
+    
+    
+    //MARK: - Dyanamic Properties
+    
+    var rollbookString: String {
+        guard let rollbook = rollbook else { return "Pledge" }
+        return "#\(rollbook)"
+    }
+    
+    var description: String {
+        return "\(firstName) \(lastName) (\(rollbookString))"
+    }
+    
+}
--- Attendance/ViewController.swift
+++ Attendance/ViewController.swift
@@ -10,17 +10,85 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var card: NSView!
+    @IBOutlet weak var image: NSImageView!
+    @IBOutlet weak var firstName: NSTextField!
+    @IBOutlet weak var lastName: NSTextField!
+
+    var currentMember: Member!
+    
+    
+    //MARK: - Setup
+    
+    override func viewDidLayout() {
+        self.view.window?.titlebarAppearsTransparent = true
+        self.view.window?.titleVisibility = .hidden
+        self.view.window?.isMovableByWindowBackground = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //set up card
+        self.view.wantsLayer = true
+        self.view.superview?.wantsLayer = true
+        self.card.wantsLayer = true
+        
+        self.card.shadow = NSShadow()
+        
+        self.card.layer?.backgroundColor = NSColor.white.cgColor
+        self.card.layer?.cornerRadius = 10.0
+        
+        self.card.layer?.shadowOpacity = 0.2
+        self.card.layer?.shadowColor = NSColor.black.cgColor
+        self.card.layer?.shadowOffset = NSMakeSize(0, 15)
+        
+        let shadowRect = CGRect(x: 0, y: -10, width: self.card.bounds.width, height: self.card.bounds.height - 10)
+        self.card.layer?.shadowPath = CGPath(roundedRect: shadowRect, cornerWidth: 10, cornerHeight: 10, transform: nil)
+        self.card.layer?.shadowRadius = 10
+        
+        //populate views
+        self.image.wantsLayer = true
+        self.image.layer?.cornerRadius = 5.0
+        
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    
+    //MARK: - Controller
+    
+    func setUpForNextMember() {
+        guard let currentIndex = Member.all.index(of: currentMember) else {
+            setUp(forMember: Member.all.first!)
         }
+        
+
     }
+    
+    func setUp(forMember member: Member) {
+        self.currentMember = member
+        
+        let image
+    }
+    
+    
+    //MARK: - User Interaction
+    
+    @IBAction func absentPressed(_ sender: NSButton) {
+    }
+    
+    @IBAction func excusedPressed(_ sender: NSButton) {
+    }
+    
+    @IBAction func presentPressed(_ sender: NSButton) {
+    }
+    
+    func processSelection(attendance: Attendance) {
+        setUp(forMember: <#T##Member#>)
+    }
+
+    
+    
+    
 
 
 }
