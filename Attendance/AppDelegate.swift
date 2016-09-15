b626e17bdc9e222e2a08da75feb2dfaefa172e1c
Cal Stephens <cal@calstephens.tech>
2016-09-15 13:43:43 -0400

    Initial Commit

---
+++ Attendance/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Attendance
+//
+//  Created by Cal Stephens on 9/15/16.
+//  Copyright © 2016 Cal Stephens. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Attendance/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Attendance
+//
+//  Created by Cal Stephens on 9/15/16.
+//  Copyright © 2016 Cal Stephens. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
